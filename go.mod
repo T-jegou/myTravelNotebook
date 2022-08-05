b3919ca68010c13002c836e7f8a075ee92b29c89
T-jegou <thomas.jegou1@gmail.com>
2022-08-06 01:13:23 +0200

    Start ORM milestone

--- go.mod
+++ go.mod
@@ -12,14 +12,32 @@ require (
 	github.com/go-openapi/validate v0.22.0
 	github.com/jessevdk/go-flags v1.5.0
 	golang.org/x/net v0.0.0-20220805013720-a33c5aa5df48
+	gorm.io/gorm v1.23.8
+)
+
+require (
+	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
+	github.com/jackc/pgconn v1.12.1 // indirect
+	github.com/jackc/pgio v1.0.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgproto3/v2 v2.3.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20200714003250-2b9c44734f2b // indirect
+	github.com/jackc/pgtype v1.11.0 // indirect
+	github.com/jackc/pgx/v4 v4.16.1 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	golang.org/x/crypto v0.0.0-20220722155217-630584e8d5aa // indirect
+	golang.org/x/text v0.3.7 // indirect
 )
 
 require (
 	github.com/asaskevich/govalidator v0.0.0-20210307081110-f21760c49a8d // indirect
+	github.com/avast/retry-go v3.0.0+incompatible
 	github.com/docker/go-units v0.4.0 // indirect
 	github.com/go-openapi/analysis v0.21.4 // indirect
 	github.com/go-openapi/jsonpointer v0.19.5 // indirect
 	github.com/go-openapi/jsonreference v0.20.0 // indirect
+	github.com/jinzhu/gorm v1.9.16
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
@@ -27,4 +45,5 @@ require (
 	go.mongodb.org/mongo-driver v1.10.1 // indirect
 	golang.org/x/sys v0.0.0-20220804214406-8e32c043e418 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gorm.io/driver/postgres v1.3.8
 )
--- pkg/handler/crud.go
+++ pkg/handler/crud.go
@@ -1,6 +1,8 @@
 package handler
 
 import (
+	"fmt"
+
 	"github.com/T-jegou/myTravelNotebook/swagger_gen/restapi/operations/travel"
 	"github.com/go-openapi/runtime/middleware"
 )
@@ -25,6 +27,7 @@ type crud struct{}
 
 // travel
 func (c *crud) GetTravel(params travel.GetTravelByIDParams) middleware.Responder {
+	fmt.Println("Salutation")
 	return nil
 }
 
@@ -32,15 +35,40 @@ func (c *crud) DeleteTravel(params travel.DeleteTravelByIDParams) middleware.Res
 	return nil
 }
 
-func (c *crud) UpdateTravel(travel.UpdateTravelByIDParams) middleware.Responder {
+func (c *crud) UpdateTravel(params travel.UpdateTravelByIDParams) middleware.Responder {
 	return nil
 }
 
 // travels
 func (c *crud) GetTravels(params travel.GetTravelsParams) middleware.Responder {
+	fmt.Println(params.HTTPRequest.Body)
 	return nil
 }
 
-func (c *crud) AddTravel(travel.AddTravelParams) middleware.Responder {
+func (c *crud) AddTravel(params travel.AddTravelParams) middleware.Responder {
+	println("Bruhh")
+
 	return nil
 }
+
+/* func (c *crud) CreateTag(params tag.CreateTagParams) middleware.Responder {
+	s := &entity.Flag{}
+	s.ID = uint(params.FlagID)
+	t := &entity.Tag{}
+	t.Value = util.SafeString(params.Body.Value)
+	if ok, reason := util.IsSafeValue(t.Value); !ok {
+		return tag.NewCreateTagDefault(400).WithPayload(ErrorMessage("%s", reason))
+	}
+
+	getDB().Where("value = ?", util.SafeString(params.Body.Value)).Find(t) // Find the existing tag to associate if it exists
+	if err := getDB().Model(s).Association("Tags").Append(t).Error; err != nil {
+		return tag.NewCreateTagDefault(500).WithPayload(ErrorMessage("%s", err))
+	}
+
+	resp := tag.NewCreateTagOK()
+	resp.SetPayload(e2r.MapTag(t))
+
+	entity.SaveFlagSnapshot(getDB(), util.SafeUint(params.FlagID), getSubjectFromRequest(params.HTTPRequest))
+	return resp
+}
+*/
